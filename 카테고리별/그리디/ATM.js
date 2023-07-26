37d4c39158e1d414af6b179816eba2ba16fc1ef3
JoSooha <dlrhdcjs@naver.com>
2023-07-26 23:19:10 +0900

    조이스틱.js 그리디 개념 적립

--- 카테고리별/그리디/ATM.js
+++ 카테고리별/그리디/ATM.js
@@ -4,10 +4,9 @@ let [N, ...rest] = fs.readFileSync(filePath).toString().trim().split("\n");
 N = Number(N);
 rest=rest[0].split(" ").map(Number)
 
-rest.sort((a,b)=>a-b);
-console.log(rest)
+rest.sort((a,b)=>a-b); //결국 정렬해서 더해주면 최종 누적합이 최소다 
 
-let total = 0;
+let total = 0; //그냥 단계별로 누적합 더해주는 로직 
 for(let i=0; i<N; i++) {
     let sum = 0;
     for(let k=0; k<=i; k++) {
--- 카테고리별/그리디/조이스틱.js
+++ 카테고리별/그리디/조이스틱.js
@@ -1,7 +1,7 @@
 // 왔다갔다 해야하는 부분 고려 못함
 function solution(name) {
-    var count1 = 0;
-    var count2 = 0;
+    var count1 = 0; //정방향
+    var count2 = 0; //역방향 
 
     var arr = [];
     for(let i=65; i<=90; i++){
@@ -12,7 +12,7 @@ function solution(name) {
         if(i>13) i=26-i;
         map.set(e, i);
     })
-    console.log(map); 
+    console.log(map); //각 알파벳마다 A에서 얼만큼 움직이면 되는지 Map 형식 저장
 
     count1 += map.get(name[0]) //첫번째는 일단 더하고
     count2 += map.get(name[0]) //첫번째는 일단 더하고
@@ -47,28 +47,28 @@ function solution(name) {
     console.log("최종2", count2) //정방향 역방향 둘 중 나은거 고르기 
     return count1 > count2 ? count2 : count1;
 }
-solution("ABAAAAAAAAABB") //7
+// console.log(solution("ABAAAAAAAAABB")) //7
 
 
 //윤코드 - 구글링 딴거 맞는게 없음 
-function solution(name) {
+function solution2(name) {
     var answer = 0;
   
     let min = name.length - 1;
-  
+    console.log(name)
     for (let i = 0; i < name.length; i++) {
       let tmp = name.charCodeAt(i);
-  
+        console.log("tmp", tmp)
       if (tmp < 78) {
         answer += tmp % 65;
       } else {
         answer = answer + 91 - tmp;
-      }
+      } //여기까지 각 char의 숫자 구함 
   
       let nextIndex = i + 1;
   
       while (nextIndex < name.length && name.charCodeAt(nextIndex) === 65)
-        nextIndex += 1;
+        nextIndex += 1; //계속 a 나오면 쭉 next 넘어가라 
   
       if (i == 0) min = Math.min(name.length - nextIndex, min);
       else min = Math.min(2 * i + name.length - nextIndex, min);
@@ -80,3 +80,5 @@ function solution(name) {
     answer = answer + min;
     return answer;
   }
+
+  console.log(solution2("JEROEN"))
