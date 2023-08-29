41fd1f14f1f73dd215305b52014b8f763bc2b76f
JoSooha <dlrhdcjs@naver.com>
2023-08-29 11:24:56 +0900

    설탕 배달.js + 체스판 refactor

--- 카테고리별/완전탐색/체스판 다시 칠하기.js
+++ 카테고리별/완전탐색/체스판 다시 칠하기.js
@@ -1,42 +1,49 @@
-const fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
-let NM = input.shift().split(' ');
-let N = Number(NM.shift());
-let M = Number(NM.shift());
+//https://jaekwan.tistory.com/110#%--%ED%-A%B-%EC%-D%B-%EC%--%AC%ED%--%AD
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let [NM, ...originArray] = fs.readFileSync(filePath).toString().trim().split("\n")
+let [N, M] = NM.split(" ").map(Number);
+originArray = originArray.map(e=>e.split("\n"))
 let white = ['WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW']
 let black = ['BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB']
 
-let board = [];
-for (let i = 0; i < N; i++) {
-  board[i] = input.shift().split('');
-}
-
-let answer = 90;
-
-for (let i = 0; i <= N - 8; i++) {
-  for (let j = 0; j <= M - 8; j++) {
-    check(j, i);
-  }
-}
-
-function check(x, y) {
-  let checkWhite = 0;
-  let checkBlack = 0;
-
-  for (let i = y; i < y + 8; i++) {
-    for (let j = x; j < x + 8; j++) {      
-      if (board[i][j] !== white[i-y][j-x])
-        checkWhite++;
-      if (board[i][j] !== black[i-y][j-x])
-        checkBlack++;
+//main에서 넘겨준 8x8의 부분 배열 검사 
+//white와 black 두 경우 중 차이가 적은 것을 리턴해줌 
+function check(j,h) { 
+    let whiteDiff = 0;
+    let blackDiff = 0;
+    for(let i=j; i<8+j; i++) {
+        for(let k=h; k<8+h; k++) {
+            if(originArray[i][0][k]!==white[i-j][k-h]) whiteDiff++;
+            if(originArray[i][0][k]!==black[i-j][k-h]) blackDiff++;
+        }
     }
-  }
-
-  let min = checkBlack < checkWhite ? checkBlack : checkWhite;
-  
-  if (min < answer)
-    answer = min;
+    return whiteDiff>blackDiff ? blackDiff : whiteDiff
 }
 
-console.log(answer);
+//오리지날 큰 배열에서 8x8 부분 배열을 check에 넘기도록 index를 넘김
+//모든 루프 돌려서 가장 작은 diff 
+function main() {
+    let min = 9999;
+    for(let j=0; j<=N-8; j++) {
+        for(let h=0; h<=M-8; h++) { 
+            let diff = check(j,h);
+            if(min>=diff) min = diff;
+        }
+    }
+    console.log(min)
+}
+
+main();
+
+//원래 야예 main에서 slice로 8x8 부분 배열로 잘라서 배열을 넘기려고 했는데 
+//배열을 넘기는 것 까다롭 + 2차원 배열을 slice 하는게 또 함수화 시켜야함 
+//오리지날 배열 하나갖다가 index로만 넘나드는게 당연히 부하도 적고 시간도 적고 
+
+
+// 이거 접근하려면 array[0][0][1] 로 해야 b 접근
+// array[0][0]이 'abcd'
+// array = [
+//     ['abcd'],
+//     ['abde']
+// ]
