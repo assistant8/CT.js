ddfb20872770881911f8289357d8647e73910237
JoSooha <dlrhdcjs@naver.com>
2023-09-19 02:50:30 +0900

    [시간초과 해결] 나이트의 이동.js

--- 카테고리별/BFS, DFS/나이트의 이동.js
+++ 카테고리별/BFS, DFS/나이트의 이동.js
@@ -21,21 +21,24 @@ function BFS(array) {
         // console.log("needVisit", needVisit)
 
         const [[p,q], depth] = needVisit.shift();
-        if(p===last[0] && q===last[1]) return depth
+        if(p===last[0] && q===last[1]) return depth //이건 여기에, push 할때 두면 예외가 있을 수도(틀림)
         const X = [-2, -2, -1, -1, 1, 1, 2, 2];
         const Y = [1, -1, 2, -2, 2, -2, 1, -1];
 
-        if(graph[p][q]===0) { //방문 안했으면 
-            graph[p][q]++;
-            for(let j=0; j<8; j++) {
-                const x = p + X[j];
-                const y = q + Y[j];
-                if(x>-1 && x<I && y>-1 && y<I) {
+        for(let j=0; j<8; j++) {
+            const x = p + X[j];
+            const y = q + Y[j];
+            if(x>-1 && x<I && y>-1 && y<I) {
+                if(graph[x][y]===0) { //
                     needVisit.push([[x, y], depth+1]);
+                    graph[x][y]++;
                 }
             }
         }
     }
 }
 
-//시간초과
+//시간초과 해결 
+//needvisit에 푸시할 때만 visited 여부 검사 후 미방문 시 푸시 + 여기서 visited 체크 (그래프)
+//원래 큰 생각없이 위 경우 + ㄹㅇ 그 노드 들어가는 코드 진입 시 = 둘 다 체크 했는데 시간초과 남
+//이유는 모르겠음 - 그게 큰 차이가 나는가..?
