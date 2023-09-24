87223114a7978e11057033ca7ae14844634eb9b5
JoSooha <dlrhdcjs@naver.com>
2023-09-24 23:59:36 +0900

    .

--- 카테고리별/완전탐색/N-Queen.js
+++ 카테고리별/완전탐색/N-Queen.js
@@ -1,43 +1,25 @@
-// const fs = require("fs");
-// const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-// let N = fs.readFileSync(filePath).toString().trim();
-// N = Number(N)
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let N = fs.readFileSync(filePath).toString().trim();
+N = Number(N)
 
-// //같은 행, 같은 열, 같은 대각선에 놓지 않기
+//같은 행, 같은 열, 같은 대각선에 놓지 않기
+//
 
-// let array = 
-// for(let i=0; i<N; i++) { //i행에
-//     for(let k=0; k<N; k++) { //i행의 k열
+//초기화 해줘야 
+let array = Array.from(Array(N), ()=>Array(N).fill(0));
 
-//     }
-// }
+outer: for(let i=0; i<N; i++) { //i행에
+    inner: for(let k=0; k<N; k++) { //i행의 k열
+        if(isValid(i, k)) array[i][k]++;
+    }
+}
 
-function solution(n) {
-    let answer = 0;
-    
-    const dfs = (board, row) => {
-      if(row === n) answer++;
-      else {
-        for(let i = 1; i <= n; i++) {
-          board[row+1] = i;
-          if(isValid(board, row+1)) dfs(board, row+1);
-        }
-      }
+function isValid(w, h) {
+    for(let j=0; j<N; j++) {
+        if(array[w][j]!==0) return false;
+        if(array[j][h]!==0) return false; 
     }
     
-    const isValid = (board, row) => {
-      for(let i = 1; i < row; i++) {
-        if(board[i] === board[row]) return false;
-        if(Math.abs(board[i] - board[row]) === Math.abs(i - row)) return false;
-      }
-      return true;
-    }
     
-    for(let i = 1; i <= n; i++) {
-      const board = new Array(n+1).fill(0);
-      board[1] = i;
-      dfs(board, 1);
-    }
-    
-    return answer;
-  }
+}
