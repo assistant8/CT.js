a794bf522dbb5fb8ef1030930c91c67c79a19beb
JoSooha <dlrhdcjs@naver.com>
2023-09-30 16:13:08 +0900

    N-Queen.js 주석 및 백트래킹 정리

--- 카테고리별/DP/퇴사.js
+++ 카테고리별/DP/퇴사.js
@@ -3,7 +3,7 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let [N, ...array] = fs.readFileSync(filePath).toString().trim().split("\n")
 array = array.map(e=>e.split(" ").map(Number))
 N = Number(N)
-console.log(array)
+// console.log(array)
 
 const dp = new Array(N).fill(0);
 
@@ -13,21 +13,25 @@ for(let i=0; i<N; i++) {
     dp[i] = dp[i] + value; //i에선 무조건 i번째 수행한다고 생각
     
     for(let k=period+i; k<N; k++) { //i 수행할 시 기간 이후 가능한 날의 일들에 대한 값 할당
-        console.log("dp[k], dp[i]", dp[k], dp[i]) 
-        dp[k] = Math.max(dp[k], dp[i]) //기존 값(i 수행 안함) vs 
+        dp[k] = Math.max(dp[k], dp[i]) //기존 값(i 수행 안함) vs i 수행한 값 
     }
 }
 
-console.log(dp)
+console.log(Math.max(...dp))
 
 
 // 원랜 2차원 배열로 각 i마다의 N크기 배열을 선언해서 했으면 이해 빨랐을텐데
 // 굳이 이전 배열이 필요가 없기에 i++ 될때 배열 재사용 
 // 위에서 물려받은 dp[i]는 i에 일할 수 있는 조건 하에 기존까지 최대수익을 보여주는 것
 // 물론 i=3일 때 dp[7]처럼 3일에 일을 안한 경우도 있음 (2일에 하고 3일에 안한)
-// https://kau-algorithm.tistory.com/800
+// https://kau-algorithm.tistory.com/800 보고 했음 
 // 탭 노트 
+
+// 이건 단순 브루트포스로 안되고 dp로 해야될 것 같은게
+// 각 경우의 수를 조합 순열화 못시키고 무한 백트랙킹 해야되는 느낌
+// 각 일을 택했을 때 이후 일을 택하는 경우가 동적으로 늘어남 
 
 // dp[n] = 이걸 선택 or 이거 안하고 할 수 있는 것들을 했을 때 값 중 큰 것 
 
 // n의 값 = n-1 값 + 현재 가능하면 현재 값 ? 틀렸던 처음 생각 
+// console.log("dp[k], dp[i]", dp[k], dp[i]) 
--- 카테고리별/완전탐색/N-Queen.js
+++ 카테고리별/완전탐색/N-Queen.js
@@ -3,23 +3,52 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let N = fs.readFileSync(filePath).toString().trim();
 N = Number(N)
 
+let answer = 0;
+for(let i=0; i<N; i++) { //i가 첫번째 줄에 몇 열에 놓을 것인가를 뜻함
+    let board = new Array(N).fill(0);
+    board[0] = i; //[i,0,0,0] 4번 반복 (첫 행에는 초기값을 줘야 vaild 검증이 되니까 이러는 듯)
+    DFS(board, 0);
+    console.log()
+}
+console.log(answer)
+
+
+function DFS(board, row) { //재귀로 넘어가기 전 한 dfs는 한 줄을 담당 
+    if(row === N-1) answer++; //재귀로 넘어왔을 때 마지막 줄이면 케이스 카운트
+    else {
+        for(let i=0; i<N; i++) { //각 줄에서 어떤 열을 선택할 지
+            board[row+1] = i;
+            console.log(board)
+
+            if(isvalid(board, row+1)) { //합당하면 다음 줄로 재귀로 넘어감
+                console.log("!!")
+                DFS(board, row+1);
+            }
+        }
+    }
+}
+
+function isvalid(board, row) { //현재의 줄까지만 검증하네 ㅇㅈ 
+    for(let i=0; i<row; i++) {
+        if(board[i]===board[row]) return false; //각 원소에 같은 수 있나
+        if(Math.abs(board[i]-board[row]) === Math.abs(i-row)) return false; 
+        //대각선 = 인덱스와 원소값의 차이가 동일한지 = 행과 열로 기울기 구함
+    }
+    return true;
+}
+
+
+
+
+
 //같은 행, 같은 열, 같은 대각선에 놓지 않기
-//
 
-//초기화 해줘야 
-let array = Array.from(Array(N), ()=>Array(N).fill(0));
+//1차원으로 한 이유 - 시간 초과 , index가 행이 된다면 ㄱㅊ, 배열 값이 같지만 않으면 열 문제는 해결 
+//(행 문제는 당연히 한 index 안에 여러 값이 안들어가면 각 행에 하나의 원소만 들어가겠지)
 
-outer: for(let i=0; i<N; i++) { //i행에
-    inner: for(let k=0; k<N; k++) { //i행의 k열
-        if(isValid(i, k)) array[i][k]++;
-    }
-}
+//대각선 체크 = 직선의 기울기의 절댓값 = y좌표 차이 / x좌표 차이
 
-function isValid(w, h) {
-    for(let j=0; j<N; j++) {
-        if(array[w][j]!==0) return false;
-        if(array[j][h]!==0) return false; 
-    }
-    
-    
-}
+//백트래킹, DFS 관련 
+//그 줄에서 vaild 해야 다음 줄로 넘어가기 때문에 valid 안한 모든 경우의 수를 탐색 안한다는 점에서 효율적 
+//valid 하자마자 다음 줄로 넘어감 (재귀) - for문 다 돌았는데 다음 재귀 안넘어가면 그 DFS 탈출하고 다음 넘어가버리지 - 그 경우 배제 
+//탭 메모 확인 - 이해하기 
