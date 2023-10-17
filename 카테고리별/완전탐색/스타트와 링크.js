e8178ba11ccf796759f4ee546d8e34fc8fb9f9ed
JoSooha <dlrhdcjs@naver.com>
2023-10-17 20:11:02 +0900

    스타트와 링크.js 치킨배달.js 및 주석 추가

--- 카테고리별/완전탐색/스타트와 링크.js
+++ 카테고리별/완전탐색/스타트와 링크.js
@@ -3,10 +3,55 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 let [N, ...array] = fs.readFileSync(filePath).toString().trim().split("\n")
 array = array.map(e=>e.split(" ").map(Number))
 
-console.log(array)
+// console.log(array)
 
 function main() {
-    
+    const indexArr = createArray(); //[0,1,2,3,4,5]
+    const combi = getCombination(indexArr, N/2); //[0,1,2], [0,4,5] ...
+    // console.log("combi", combi);
+    let least = 10000; //일단 최솟값 세팅 
+
+    for(let i=0; i<combi.length; i++) { //각 콤비의 경우의 수
+        const combi1 = combi[i]; //기존 콤비 3개
+        const combi2 = getRestCombi(combi[i]); //나머지 3개
+        // console.log("restCombi", combi1, combi2)
+        const redArr = getCombination(combi1, 2); //콤비1에 대한 원소 2개인 콤비들 (2차원 배열이니 x,y 좌표)
+        const blueArr = getCombination(combi2, 2); //콤비2에 대한 원소 2개 콤비들
+        
+        // console.log("blueArr", redArr, blueArr)
+        let redSum = 0; //각 원소 2개 콤비에 대한 합 
+        let blueSum = 0;
+        for(let k=0; k<redArr.length; k++) { //합을 구하는 과정 
+            const [a,b] = redArr[k];
+            const [c,d] = blueArr[k];
+            redSum += array[a][b];
+            redSum += array[b][a];
+            blueSum += array[c][d];
+            blueSum += array[d][c];
+        }
+        // console.log("redSum, blueSum", redSum, blueSum)
+        // 두 합의 차와 현재 최소값과 비교해 최소값 업뎃 
+        if(least > Math.abs(redSum-blueSum)) least = Math.abs(redSum-blueSum)
+    }
+    return least;
+
+}
+console.log(main())
+
+function getRestCombi(originCombi) {
+    let result = []; 
+    for(let i=0; i<N; i++) {
+        if(!originCombi.includes(i)) result.push(i);
+    }
+    return result;
+}
+
+function createArray() {
+    let result = []; 
+    for(let i=0; i<N; i++) {
+        result.push(i);
+    }
+    return result;
 }
 
 function getCombination(array, selectNumber) {
@@ -24,5 +69,8 @@ function getCombination(array, selectNumber) {
     return results
 }
 
+
 // N 중에 N/2를 구하는 조합을 구해서 
 // 나머지 조합과의 각각 합을 구한다
+
+//결국 모든 조합의 경우의 수 구해 진행 
--- 카테고리별/완전탐색/치킨배달.js
+++ 카테고리별/완전탐색/치킨배달.js
@@ -1,50 +1,77 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let [MN, ...array] = fs.readFileSync(filePath).toString().trim().split("\n")
+array = array.map(e=>e.split(" ").map(Number))
+const [N, M] = MN.split(" ").map(Number)
 
-const [n, m] = input.shift().split(' ').map(Number);
-const city = input.map((line) => line.split(' ').map(Number));
-const house = [];
-const chicken = [];
+//2의 개수들 중 m개를 뽑는 콤비네이션을 구함
+//그 콤비네이션이 깔린 상태에서의 각 1에서의 bfs 해서 최소거리 구하고 그 합을 구함 
+//그 합 중 최솟값을 구해 
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (city[i][j] === 1) house.push([i, j]);
-    else if (city[i][j] === 2) chicken.push([i, j]);
-  }
+//굳이라고 생각해서 bfs가 아닌 for문으로 getDistance 해서 최소 거리를 구함 
+
+function main() {
+    const allChickenArray = getChickenCoord();
+    const chickenCoordCombi = getCombination(allChickenArray, M);
+    // console.log("chickenCoordCombi", chickenCoordCombi)
+    const allHouseCoord = getHouseCoord();
+    // console.log("allHouseCoord", allHouseCoord)
+
+    let leastDistanceInChickenCombi = 99999;
+    for(let i=0; i<chickenCoordCombi.length; i++) { //각 치킨 위치 후보들에 대해 진행 
+        //bfs 안하고 for문으로 getDistance로 치킨집과 최소값 구하기로함
+        let allHouseDistanceSum = 0;
+        for(let k=0; k<allHouseCoord.length; k++) { //각 집마다의 계산 
+            let least = 99999;
+            for(let j=0; j<chickenCoordCombi[i].length; j++) { //해당 후보의 각 치킨집 
+                const distance = getDistance(chickenCoordCombi[i][j], allHouseCoord[k]);
+                if(distance<least) least = distance;
+            }
+            allHouseDistanceSum+=least;
+        }
+        if(leastDistanceInChickenCombi>allHouseDistanceSum) leastDistanceInChickenCombi=allHouseDistanceSum
+    }
+    console.log(leastDistanceInChickenCombi)
+}
+main();
+
+function getHouseCoord() {
+    const result = [];
+    for(let i=0; i<N; i++) {
+        for(let k=0; k<N; k++) {
+            if(array[i][k]===1) result.push([i,k])
+        }
+    }
+    return result;
 }
 
-const getMinDistance = () => {
-  let sum = 0;
-  house.forEach(([hx, hy]) => {
-    let min = Infinity;
-    chicken.forEach((_, index) => {
-      if (check[index] === true) {
-        const [cx, cy] = chicken[index];
-        min = Math.min(min, Math.abs(hx - cx) + Math.abs(hy - cy));
-      }
-    });
-    sum += min;
-  });
-  return sum;
-};
-
-const check = new Array(chicken.length).fill(false);
-let answer = Infinity;
-
-const DFS = (idx, cnt) => {
-  if (cnt === m) {
-    answer = Math.min(answer, getMinDistance());
-    return;
-  } else {
-    for (let i = idx; i < chicken.length; i++) {
-      if (check[i] === true) continue;
-      check[i] = true;
-      DFS(i, cnt + 1);
-      check[i] = false;
+function getChickenCoord() {
+    const result = [];
+    for(let i=0; i<N; i++) {
+        for(let k=0; k<N; k++) {
+            if(array[i][k]===2) result.push([i,k])
+        }
     }
-  }
-};
+    return result;
+}
 
-DFS(0, 0);
-console.log(answer);
+function getDistance(coord1, coord2) {
+    return Math.abs(coord1[0]-coord2[0]) + Math.abs(coord1[1]-coord2[1])
+}
+
+function getCombination(array, selectNumber) {
+    const results = [];
+    if(selectNumber===1) {
+        return array.map(element=>[element])
+    }
+
+    array.forEach((fixed, index, origin)=>{
+        const rest = origin.slice(index+1);
+        const combinations = getCombination(rest, selectNumber-1);
+        const attached = combinations.map(combination=>[fixed, ...combination]);
+        results.push(...attached);
+    })
+
+    return results;
+}
+
