b824c684f5696b4e0e6cc3c8c57429c9a52985c5
JoSooha <dlrhdcjs@naver.com>
2023-11-09 02:24:07 +0900

    체육복.js 새로운 풀이

--- 카테고리별/그리디/체육복.js
+++ 카테고리별/그리디/체육복.js
@@ -24,3 +24,42 @@ function solution(n, lost, reserve) {
 
 
 //-1부터 시작해야 선점 후 break 되어서 최대값이 나올 듯
+
+
+
+
+
+
+//////////////////
+//231103 새로운 풀이
+
+// 1로 초기화 (n+1)
+// lost 돌면서 -1, reserve 돌면서 +1
+// 이 arr 돌면서 0이면 양쪽에 2인지 보고 빌리고 -1 해줌 
+// idx 0은 예외 처리 
+function solution(n, lost, reserve) {
+    var answer = 0;
+    const arr = new Array(n+1).fill(1); //1으로 초기화
+    lost.forEach(e=>{
+        arr[e]--;
+    })
+    reserve.forEach(e=>{
+        arr[e]++;
+    })
+    
+    for(let i=1; i<=n; i++) {
+        if(arr[i]===0) {
+            if(arr[i-1]===2 && i>1) {
+                arr[i-1]--;
+                arr[i]++;
+                continue;
+            } else if(arr[i+1]===2 && i<n) {
+                arr[i+1]--;
+                arr[i]++;
+                continue;
+            }
+        }
+    }
+    arr.forEach(e=>{if(e>0) answer++})
+    return answer-1;
+}
