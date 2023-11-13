378f773eb9135c1f1a1cb04938018e2d3e2165c6
JoSooha <dlrhdcjs@naver.com>
2023-11-13 23:09:01 +0900

    불안한 무빙워크.js 성공

--- 카테고리별/구현/불안한 무빙워크.js
+++ 카테고리별/구현/불안한 무빙워크.js
@@ -10,15 +10,20 @@ function main() {
     while(1) {
         count++;
         first();
+        console.log("1origin", origin)
+        console.log("1people", people)
         second();
+        console.log("2origin", origin)
+        console.log("2people", people)
         third();
-        // console.log("origin", origin)
-        // console.log("people", people)
+        console.log("3origin", origin)
+        console.log("3people", people)
         if(origin.filter(e=>e===0).length >= k) return count;
     }
 }
 console.log(main())
 
+//people은 index 맞으니 0 포함 여부 확인 맞음 
 function third() {
     if(!people.includes(0) && origin[0]!==0) {
         people.push(0);
@@ -26,10 +31,12 @@ function third() {
     }
 }
 
+//먼저 올라간 사람부터 = people 0부터 맞음 
+//앞선 칸 사람이 다음칸 안정성 0 이라 안간경우도 있으니 앞선칸 사람 존재 고려 
 function second() {
     for(let i=0; i<people.length; i++) {
         const idx = people[i];
-        if(origin[idx+1]!==0) {
+        if(origin[idx+1]!==0 && people[i-1]!==idx+1) {
             origin[idx+1]--;
             people[i]++;
         }
@@ -46,12 +53,21 @@ function first() {
     }
 }
 
+//people이 push되는 형식이라 오래된거가 왼에 있는걸 망각함 - 콘솔찍어서 깨달음
 function isArrive() {
-    if(people.includes(n-1)) {
-        const index = people.indexOf(n-1);
-        people.splice(index, 1);
+    if(people.length > 0 && people[0] === n-1) {
+        people.shift();
     }
 }
+
+// includes, indexof 등 시간복잡도 높다고 함 
+
+// function isArrive() {
+//     if(people.includes(n-1)) {
+//         const index = people.indexOf(n-1);
+//         people.splice(index, 1);
+//     }
+// }
 
 //1번은 따로 함수 빼서 원소와 사람(원본 배열, 사람 배열) 모두 한칸씩 전진 
     //전진 = 원본은 pop해서 그걸 unshift, 사람 배열은 모두 +1 (n보다 크면 pop)
