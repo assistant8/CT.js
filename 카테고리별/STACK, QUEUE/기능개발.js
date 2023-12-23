e40c17a5dee8604d0a085c4b76275d9527bea71b
JoSooha <dlrhdcjs@naver.com>
2023-12-24 00:59:23 +0900

    기능 개발.js 다시 풀어본 풀이

--- 카테고리별/STACK, QUEUE/기능개발.js
+++ 카테고리별/STACK, QUEUE/기능개발.js
@@ -23,3 +23,67 @@ function solution(progresses, speeds) {
     }
     return answer;
 }
+
+// 231223 다시 풀어본 풀이 
+function solution(progresses, speeds) {
+    var answer = [];
+    const length = speeds.length;
+    let remain = progresses.map(e => 100-e);
+    let finish = Array(length).fill(0);
+    for(let i=0; i<length; i++) {
+        const node = Math.ceil(remain[i]/speeds[i]);
+        finish[i] = node;
+    }
+    
+    for(let i=0; i<length; i++) {
+        if(finish[i]<finish[i+1]) answer.push(1);
+        else {
+            const count = delay(i, finish[i]);
+            answer.push(count)
+            i+=(count-1);
+        }
+    }
+    
+    function delay(stop, norm) {
+        let count = 1;
+        for(let k=stop+1; k<length; k++) {
+            if(norm >= finish[k]) count++;
+            else return count;
+        }
+        return count;
+    }
+    
+    return answer;
+}
+
+// 기존 for문을 진행하다가 특정 조건에서 거기서 또 다른 for문을 통해 체크를 하는 등의 수행이 있는
+// 같은 방향으로 for문이 하나씩 진행되긴함, 그러면 2번 for문에서 진행된 정도를 1번 for문에 적용 (i=k+count) 해야되는.. 불편함이 생기는데 
+
+// 아래는 위 방법에서 i i+1 대소 관계에 상관없이 무조건 norm을 지정해서 그것과 비교하는 로직으로 통일함
+
+function solution(progresses, speeds) {
+    let answer = [];
+    const length = speeds.length;
+    let remain = progresses.map(e => 100-e);
+    let finish = Array(length).fill(0);
+    for(let i=0; i<length; i++) {
+        const node = Math.ceil(remain[i]/speeds[i]);
+        finish[i] = node;
+    }
+    
+    let norm = finish[0];
+    let count = 1;
+    
+    for(let i=1; i<length; i++) {
+        if(norm >= finish[i]) {
+            count++;
+        } else {
+            norm = finish[i];
+            answer.push(count);
+            count = 1;
+        }
+    }
+    answer.push(count)
+
+    return answer;
+}
