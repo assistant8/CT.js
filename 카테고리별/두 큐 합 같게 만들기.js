90c0b84c65703020b156a4716abb8b0a7d637c4f
JoSooha <dlrhdcjs@naver.com>
2024-01-19 23:58:19 +0900

    두 큐 합 같게 만들기.js 투 포인터 버전

--- 카테고리별/두 큐 합 같게 만들기.js
+++ 카테고리별/두 큐 합 같게 만들기.js
@@ -47,3 +47,37 @@ console.log(solution([1,1], [1,5]))
 //그러면 두 배열 중 큰 쪽에서 빼와서 작은 쪽에 넣어주는게 맞다 - 이거말곤 없을 듯 - 근데 이걸 최적화 해보자 
 
 //투포인터로 다들 푼다, 이걸로도 풀어보자 
+function solution(queue1, queue2) {
+    const queue = [...queue1, ...queue2];
+    let sq1 = queue1.reduce(sum, 0);
+    let sq2 = queue2.reduce(sum, 0);
+    
+    if (sq1 === sq2) return 0;
+    
+    const t_sum = (sq1+sq2);
+
+    if (t_sum%2 != 0) return -1;
+    
+    const h_sum = t_sum/2;
+    let start = 0;
+    let end = queue1.length;
+    let cnt = 0;
+    
+    while(cnt <= queue.length*3) {
+        if (h_sum === sq1) {
+            return cnt;
+        } else if (h_sum > sq1){
+            sq1 += queue[end];
+            end++;
+        } else {
+            sq1 -= queue[start];
+            start++;
+        }
+        
+        cnt++;
+    }
+    
+    return -1;
+}
+
+const sum = (a, b) => a + b;
