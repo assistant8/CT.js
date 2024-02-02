f404e7f528d827f5675136b5ce2ddbc7e6f8ef19
assistant8 <101612514+assistant8@users.noreply.github.com>
2024-02-02 17:31:39 +0900

    [level 2] Title: 양궁대회, Time: 1.71 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -25,37 +25,31 @@ function solution(n, info) {
     }
     dfs(0);
 
+    // 반복문으로 돌면서 각 경우의 수 탐색
     let max = -1;
     let maxArr = [-1];
-    // 반복문으로 돌면서 각 경우의 수 탐색
-    outer: for(let i=0; i<result.length; i++) {
+    for(let i=0; i<result.length; i++) {
         const onecase = result[i]; //하나의 케이스
         let arrowsum = 0; //n 이하여야 함
-        let sumScore1 = 0; //이 중 최댓값 구하기
+        let sumScore1 = 0; //이 둘의 차이값 중 최대 구하기 
         let sumScore2 = 0;
         let tempArr = [];
         for(let stage=10; stage>=0; stage--) { 
             let arrow = onecase[10-stage];
             if(stage===0) {
                 arrow = n-arrowsum; //남은거에 다쓰기
-                tempArr.push(arrow);
             }
-            else if(arrowsum + arrow <= n) {
-                tempArr.push(arrow);
-            }
-            else {
-                arrow = 0;
-                tempArr.push(arrow);
-            }
+            else if(arrowsum + arrow > n) arrow = 0; //넘치면 나머지 케이스인 0개
+            tempArr.push(arrow);
             arrowsum += arrow;
 
-            if(arrow===0 && info[10-stage]===0);
+            if(arrow===0 && info[10-stage]===0); //sumscore 양쪽 계산
             else if(arrow > info[10-stage]) sumScore1+=stage;
             else if(arrow <= info[10-stage]) sumScore2+=stage;
         }
         
         // 차이가 최대인 케이스, 그 중 가장 작은 값을 많이 맞춘 케이스 리턴
-        // 
+        // 최대면 바로 반영, 같으면 그때야 가장 작은 값 있는지 판단해야 (한꺼번에 불가능)
         const diff = sumScore1-sumScore2;
         if(max < diff) {
             max = diff;
