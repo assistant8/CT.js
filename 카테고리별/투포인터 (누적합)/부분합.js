1c468d59c08c8dc09bfecb740bff509e0b0ed536
JoSooha <dlrhdcjs@naver.com>
2024-04-29 22:22:21 +0900

    부분합.js 다시풀기

--- 카테고리별/투포인터 (누적합)/부분합.js
+++ 카테고리별/투포인터 (누적합)/부분합.js
@@ -13,29 +13,62 @@ array = array.split(" ").map(Number);
 // left++ 했으면 sum-array[left], right++ 이면 sum+array[right]
 
 function solution() {
-    let left = 0;
-    let right = 0;
-    let min = Infinity;
-    let sum = array[0];
+  let left = 0;
+  let right = 0;
+  let min = Infinity;
+  let sum = array[0];
 
-    while(right < N) { //right가 범위 벗어나면 끝 
-        if(sum >= S) {
-            let leng = right-left+1;
-            if(leng < min) min = leng;
-            sum -= array[left];
-            left++;
-        } else {
-            right++;
-            sum += array[right];
-        }
+  while (right < N) {
+    //right가 범위 벗어나면 끝
+    if (sum >= S) {
+      let leng = right - left + 1;
+      if (leng < min) min = leng;
+      sum -= array[left];
+      left++;
+    } else {
+      right++;
+      sum += array[right];
     }
-    return (min == Infinity ? 0 : min);
+  }
+  return min == Infinity ? 0 : min;
 }
 
-console.log(solution());
+// console.log(solution());
 
-// 연속 수열합 문제임 
+// 연속 수열합 문제임
 // 기존 생각과 달리 left가 하나 증가할 때마다 right = left 부터 시작할 필요가 없다
 // 그러면 부분합의 의미가 없어짐 - 그 left 단계에서 부분합을 구하기 위한 루프가 하나 더 필요
 // 부분합 노트 그림 참고해봐라 이전 단계에서 한 걸로 커버됨
 // 그래서 단순히 타겟보다 작으면 right 늘리고 크면 left 늘리고
+
+function solution2() {
+  const sumArr = Array(N + 1).fill(0);
+  sumArr[0] = array[0];
+
+  for (let i = 1; i < N; i++) {
+    const num = array[i];
+    sumArr[i] = sumArr[i - 1] + num;
+  }
+
+  let left = (right = 0);
+  let answer = Infinity;
+
+  while (right < N) {
+    const sum = sumArr[right] - (left - 1 < 0 ? 0 : sumArr[left - 1]);
+
+    if (sum < S) {
+      right++;
+    } else {
+      if (answer > right - left) answer = right - left + 1;
+      left++;
+    }
+  }
+
+  return (answer === Infinity ? 0 : answer)
+}
+
+solution2();
+
+//처음 sol과의 차이는 누적합을 구해놓고 반복문에서 -를 통해 sum을 구하느냐,
+//누적합 안 구하고 초기 sum=arr[0]에서 left right++ 시마다 각 arr 원소 sum에 더해주냐
+
