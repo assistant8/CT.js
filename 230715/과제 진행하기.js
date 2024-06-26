835e4271f1e80779fa6df96fec25837e12b31a30
JoSooha <dlrhdcjs@naver.com>
2024-06-26 13:50:47 +0900

    과제 진행하기.js 시간/task스택/구현

--- 230715/과제 진행하기.js
+++ 230715/과제 진행하기.js
@@ -45,3 +45,72 @@ function solution(plans) {
 }
 
 console.log(solution([["science", "12:40", "50"], ["music", "12:20", "40"], ["history", "14:00", "30"], ["computer", "12:30", "100"]]))
+
+
+///////////////////////////////240626 재풀이 성공
+function solution(plans) {
+    const answer = [];
+    const leng = plans.length;
+    
+    for(let i=0; i<leng; i++) {
+        const [subject, start, playtime] = plans[i];
+        const [hour, minute] = start.split(":").map(Number);
+        const converted = hour*60 + minute;
+        plans[i][1] = converted;
+        plans[i][2] = Number(playtime);
+    }
+    plans.sort((a,b)=>a[1] - b[1]);
+    console.log(plans);
+    
+    const stack = []; // [과목,남은시간]
+    let currenttime;
+
+    // playtime을 다음꺼의 start 될때까지 차감 + 1이상이면 stack에 쌓음
+    for(let i=0; i<leng; i++) {
+        const [subject, start, playtime] = plans[i];
+        const nextstart = i===leng-1 ? Infinity : plans[i+1][1]; //다음 과목 시작시간, 마지막이면 무한대
+        const time = playtime - (nextstart - start); // 이번 과목의 남은 시간 (0이하면 다끝낸거임)
+        if(time > 0) { // 이번꺼 다 못끝내고 바로 다음꺼 start한 경우
+            stack.push([subject, time]);
+            currenttime = nextstart;
+            continue;
+        } else { //이번꺼 다 끝낸 경우
+            currenttime = start + playtime;
+            answer.push(subject); 
+        }
+
+        while(stack.length) {
+            // console.log(i, currenttime, stack)
+            const resttime = nextstart - currenttime; //다음 시작까지 남은 시간
+            if(resttime > 0) {
+                const [subject, time] = stack.pop();
+                const subresttime = time - resttime; //해당 과목 남은 시간
+                if(subresttime > 0) { //과목 안끝났다면 
+                    currenttime += time; //현재 시간 업데이트
+                    stack.push([subject, subresttime]); // 다시 남은시간 고려해서 스택 푸시 
+                } else { //과목 끝났다면
+                    currenttime += time;
+                    answer.push(subject); //과목 끝났으면
+                }
+            } else break; //시간 다됐으면
+        }
+    }
+    
+    return answer;
+}
+
+// 시간문제에서 시/분으로 나오면 분단위로 초기화하여 계산
+// 과목 별 분단위로 전환 후 시작시간 정렬
+
+
+// 매초마다 반복문 돌리면서 start==t인게 있는지 확인??
+    // let current = "";
+    // let idx = 0;
+    // let next = idx+1;
+    // for(let t=1; t<=1540; t++) {
+    //     const [subject, start, playtime] = plans[idx];
+    //     if(start === t) {
+    //         current = subject;
+    //         idx++;
+    //     }
+    // }
